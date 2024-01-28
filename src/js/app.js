939e24d175613106fc5d49a858f1105c3516efc2
Vyacheslav Plotnikov <vyacheslavw84@gmail.com>
2024-01-28 22:57:35 +0300

    new development

--- src/js/app.js
+++ src/js/app.js
@@ -64,7 +64,7 @@ flsFunctions.isWebp();
 // flsFunctions.showMore();
 
 /*
-Модуль "К/После"
+Модуль "До/После"
 Документация: https://template.fls.guru/template-docs/modul-do-pislia.html
 Сниппет (HTML): ba
 */
@@ -202,7 +202,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 // Плавная навигация по странице
 // Документация: https://template.fls.guru/template-docs/modul-plavnoj-navigacii-po-stranice.html
-// flsScroll.pageNavigation();
+ flsScroll.pageNavigation();
 
 // Функционал добавления классов к хедеру во время прокрутки
 // Документация: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
